f99b199bad798e5b3855be2777a736f3b552cbcd
simplilearn-chl <chuck.lowery@gmail.com>
2021-02-07 18:08:49 -0700

    Inital Commit

---
+++ src/main/java/com/piedpiper/webapp/WebappApplication.java
@@ -0,0 +1,21 @@
+package com.piedpiper.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@SpringBootApplication
+public class WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApplication.class, args);
+	}
+
+    @GetMapping("/")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+	return String.format("Hello %s!", name);
+    }
+    
+}
